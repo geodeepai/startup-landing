a5a45b695835a4403b33706b9edfe9453a4d4a54
unacademy.mail <unacademy.mail@gmail.com>
2026-06-12 16:56:24 +0530

    add NextAuth Google and GitHub OAuth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId:     process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId:     process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/sign-in",
+  },
+  callbacks: {
+    async redirect() {
+      return "/dashboard";
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
